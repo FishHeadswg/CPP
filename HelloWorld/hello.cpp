cf415e29e15580b1f85e5bbde33835dc0f8c32fb
FishHeadswg <fishheads@gmail.com>
2013-12-06 04:30:50 -0500

    HelloWorld

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,15 @@
+/***********************
+Trevor Richardson
+Assignment 1
+***********************/
+
+#include <iostream>  // allows program to perform input and output
+
+int main()
+{
+	std::cout << "Hello World!\a\a" << std::endl;  // Output to the screen
+	std::cout << "My Name Is Trevor Richardson" << std::endl;  // Output to the screen
+  std::cin.get();
+
+	return 0;
+}
